d05766ceff515955d1b68a8758d70dde8276953d
maxiignaciio <max.garrido@duocuc.cl>
2022-06-14 21:52:02 -0400

    Entrega 3 finalizada
    Co-authored-by: bennminn <bennminn@users.noreply.github.com>

--- Tienda_Mascotas/Tienda/Usuario/urls.py
+++ Tienda_Mascotas/Tienda/Usuario/urls.py
@@ -13,4 +13,10 @@ urlpatterns = [
     
     path('login/', auth_views.LoginView.as_view(template_name='Usuario/menu_login.html'), name="menu_login"),
     path('sobre_nosotros', TemplateView.as_view(template_name='Paginas/sobre_nosotros.html'), name='sobre_nosotros'),
+    path('correa_arena', TemplateView.as_view(template_name='Paginas/pagina_correa_arena.html'), name='correa_arena'),
+    path('cama_sofa_azul', TemplateView.as_view(template_name='Paginas/pagina_cama_sofa_azul.html'), name='cama_sofa_azul'),
+    path('collar', TemplateView.as_view(template_name='Paginas/pagina_collar.html'), name='collar'),
+    path('plato_nordico', TemplateView.as_view(template_name='Paginas/plato_nordico.html'), name='plato_nordico'),
+    path('peluches_juguetes', TemplateView.as_view(template_name='Paginas/peluches_juguetes.html'), name='peluches_juguetes'),
+    path('collar_bandana', TemplateView.as_view(template_name='Paginas/collar_bandana.html'), name='collar_bandana'),
 ]  
