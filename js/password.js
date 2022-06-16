bdbe581de907ca3d28c64ccb952519325bf30ebb
maxiignaciio <101838260+maxiignaciio@users.noreply.github.com>
2022-06-16 17:18:03 -0400

    Delete js directory

--- js/password.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var x = document.getElementById("password");
-    if (x.type === "password") {
-      x.type = "text";
-    } else {
-      x.type = "password";
-    }
-  }
